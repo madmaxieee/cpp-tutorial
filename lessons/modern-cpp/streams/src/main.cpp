1ee6786057b86281494d34e7c8f005aed799af1a
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-06-29 15:00:07 +0800

    add c style string manipulation

--- lessons/modern-cpp/streams/src/main.cpp
+++ lessons/modern-cpp/streams/src/main.cpp
@@ -1,7 +1,15 @@
+#include <_stdio.h>
+#include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <sstream>
 
 int main(int argc, char *argv[]) {
+  // c way
+  char buffer[100];
+  snprintf(buffer, sizeof(buffer), "I am %d years old.\n", 20);
+  printf("%s", buffer);
+  // cpp way
   std::ostringstream stream;
   stream << "I am " << 20 << " years old.";
   std::cout << stream.str() << std::endl;
@@ -12,6 +20,13 @@ int main(int argc, char *argv[]) {
   int age;
   stream2 >> age;
   std::cout << "age: " << age << std::endl;
+
+  int number;
+  std::cout << "reading numbers.txt" << std::endl;
+  std::ifstream file("./numbers.txt");
+  while (file >> number) {
+    std::cout << number << std::endl;
+  }
 
   return 0;
 }
